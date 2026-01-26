4ba28cc8169ae6605af5a089dbc6e87f21be42ee
yixiao.xu.7e9 <yixiao.xu.7e9@itb.cat>
2026-01-26 08:48:03 +0100

    AAAAAa

---
+++ src/AmitatsDificils.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val rep = readln().toInt()
+    repeat(rep){
+        var num = readln().toLong()
+        var base = 2L
+        var result = 1L
+        while (num>0){
+            if (num%2!=0L){
+                result *= base
+            }
+            base *= base
+            num /= 2
+        }
+        println(result)
+    }
+}
